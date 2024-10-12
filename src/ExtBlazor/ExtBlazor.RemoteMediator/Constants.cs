812082449cbb6b52835075ab7e5199ebcd49fa51
Daniel Johansson <c01danjo@live.se>
2024-10-12 19:12:58 +0200

    Fixed default url

--- src/ExtBlazor/ExtBlazor.RemoteMediator/Constants.cs
+++ src/ExtBlazor/ExtBlazor.RemoteMediator/Constants.cs
@@ -1,5 +1,5 @@
 ﻿namespace ExtBlazor.RemoteMediator;
 public static class Constants
 {
-    public const string DEFAULT_ENDPOINT_URL = "";
+    public const string DEFAULT_ENDPOINT_URL = "/api/remotemediator";
 }
